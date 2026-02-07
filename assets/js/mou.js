1b4be6336a101e6a89d26f3d72bfd8399dafce80
HarishSRM2025 <harishsrm25@gmail.com>
2026-02-07 15:23:02 +0530

    updated

--- assets/js/mou.js
+++ assets/js/mou.js
@@ -1,15 +1,5 @@
 const new_activities = document.getElementById('new_activities');
-const activity_content=[
-    {
-        title:"Title-1",
-    },
-    {
-        title:"Title-2",
-    },
-    {
-        title:"Title-3",
-    }
-]
+
 for(let i=1;i<=36;i++){
     new_activities.innerHTML+=
     `<div class="collab-card">
